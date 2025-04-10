409c85856fab2f7f35fdadebba30b80cb8b62b16
António Lopes <still_running_uphill@protonmail.com>
2025-04-10 10:30:25 +0100

    Added hasshing and authentication helper methods

--- main.py
+++ main.py
@@ -1,19 +1,73 @@
 from fastapi import FastAPI, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from typing import Optional
+
+SECRET_KEY = ""
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+
+db = {
+    "pedro": {
+        "username": "Tau123",
+        "full_name": "Pedro Pascal",
+        "email": "pedro123pascal@gmail.com",
+        "hashed_password": "",
+        "disabled": False
+    }
+}
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
+
+class User(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+    disabled: Optional[bool] = None
+
+class UserInDB(User):
+    hashed_password: str
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
 
-class Data(BaseModel):
-    name: str
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
 
-
-@app.post("/create/")
-async def create(data: Data):
-    return {"data": data}
-
-
-@app.get("/test_endpoint/{item_id}/")
-async def test_endpoint(item_id: str, query: int = 1):
-    return {"message": "Test endpoint is working!"}
+def get_user(db, username: str) -> UserInDB | None:
+    if username in db:
+        user_data = db[username]
+        return UserInDB(**user_data)
+    
+def authenticate_username(db, username: str, password: str):
+    user = get_user(db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now() + expires_delta
+    else:
+        expire = datetime.now() + timedelta(minutes=15)
+    
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
---
+++ test_api_setup.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Data(BaseModel):
+    name: str
+
+
+@app.post("/create/")
+async def create(data: Data):
+    return {"data": data}
+
+
+@app.get("/test_endpoint/{item_id}/")
+async def test_endpoint(item_id: str, query: int = 1):
+    return {"message": "Test endpoint is working!"}
