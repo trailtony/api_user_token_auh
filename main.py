e16d36f0f12ce84673d13e081d318dd981f144be
António Lopes <still_running_uphill@protonmail.com>
2025-04-10 14:45:51 +0100

    Added token access, users/me and users/me/items roots

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
-from typing import Optional
+from typing import Optional, Any
 
 SECRET_KEY = ""
 ALGORITHM = "HS256"
@@ -54,13 +54,14 @@ def get_user(db, username: str) -> UserInDB | None:
         user_data = db[username]
         return UserInDB(**user_data)
     
-def authenticate_username(db, username: str, password: str):
+def authenticate_user(db, username: str, password: str):
     user = get_user(db, username)
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
         return False
     
+
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
     to_encode = data.copy()
     if expires_delta:
@@ -72,23 +73,25 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     credential_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
     try:
         payload: dict = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
+        username = payload.get("sub")
         if username is None:
             raise credential_exception
         
-        token_data = TokenData(username=username)
+        token_data: Any = TokenData(username=username)
     except JWTError:
         raise credential_exception
     
-    user = get_user(db, username=token_data.username)
+    user = get_user(db=db, username=token_data.username)
     if user is None:
         raise credential_exception
     
     return user
+
 
 async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)) -> UserInDB:
     if current_user.disabled:
@@ -96,3 +99,21 @@ async def get_current_active_user(current_user: UserInDB = Depends(get_current_u
     
     return current_user
              
+
+@app.post("/token", response_model=Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"})
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+@app.get("/users/me", response_model=User)
+async def read_users_me(current_user: User = Depends(get_current_active_user)) -> User:
+    return current_user
+
+@app.get("/users/me/items")
+async def read_own_items(current_user: User = Depends(get_current_active_user)) -> list[dict[str, Any]]:
+    return [{"item_id": 1, "owner": current_user}]
