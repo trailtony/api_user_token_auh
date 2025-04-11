d3f1ce1856527f92ec67d554b9027b74d51a2ae5
António Lopes <still_running_uphill@protonmail.com>
2025-04-11 15:26:11 +0100

    Added Access Token Authorization test

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Authenticate Users with FastAPI and Token Authentication
-
 - A quick step by step on how to authenticate users with FastAPI and Token Authentication.
-- Based on FASTAPI documentation website: https://fastapi.tiangolo.com/tutorial/security/first-steps.
+- Based on FASTAPI documentation website: https://fastapi.tiangolo.com/tutorial/security/oauth2-jwt/#hash-and-verify-the-passwords.
 - Based on TWT's video tutorial https://www.youtube.com/watch?v=5GxQ1rLTwaU
+
+## Setup uvicorn development server
+- Run the following command to startup application: 
+    $ uvicorn main: app --reload
+- Open web page http://localhost:8000/docs to see your API root endpoints.
+ 
+## Mock data
+- Mock DB and API configuration variables are explicitly available inside main.py for example purposes.
+
+## Errors on passlib due to bcrypt's newer versions
+-  bcrypt's version has to be pinned at older versions until they fix a "AttributeError: module 'bcrypt' has no attribute '__about__'" bug in passlib package.
--- main.py
+++ main.py
@@ -4,20 +4,20 @@ from pydantic import BaseModel
 from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
-from typing import Optional, Any
+from typing import Any, Literal
 
-SECRET_KEY = ""
+SECRET_KEY = "8ae4de7579cbb794d36595ddb8820d13650ced20b22997a3cea599ca476d5438" # openssl rand -hex 32 (generate sample key)
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 
 db = {
-    "pedro": {
-        "username": "Tau123",
-        "full_name": "Pedro Pascal",
-        "email": "pedro123pascal@gmail.com",
-        "hashed_password": "",
+    "tom": {
+        "username": "tom",
+        "full_name": "Tom Pascal",
+        "email": "thomas123pascal@gmail.com",
+        "hashed_password": "$2b$12$u.AuHasdA6rjq8XjNzmJRejxO30cnmYeWqEBjwcoXoPK.hQJzr7JW",
         "disabled": False
     }
 }
@@ -27,13 +27,13 @@ class Token(BaseModel):
     token_type: str
 
 class TokenData(BaseModel):
-    username: Optional[str] = None
+    username: str | None = None
 
 class User(BaseModel):
     username: str
-    email: Optional[str] = None
-    full_name: Optional[str] = None
-    disabled: Optional[bool] = None
+    email: str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
 
 class UserInDB(User):
     hashed_password: str
@@ -54,15 +54,18 @@ def get_user(db, username: str) -> UserInDB | None:
         user_data = db[username]
         return UserInDB(**user_data)
     
-def authenticate_user(db, username: str, password: str):
-    user = get_user(db, username)
+def authenticate_user(db, username: str, password: str) -> UserInDB | Literal[False]:
+    user: UserInDB | None = get_user(db, username)
     if not user:
         return False
-    if not verify_password(password, user.hashed_password):
+    if not verify_password(plain_password=password, hashed_password=user.hashed_password):
         return False
     
+    return user
+    
+    
 
-def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.now() + expires_delta
@@ -93,7 +96,7 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
     return user
 
 
-async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)) -> UserInDB:
+async def get_current_active_user(current_user: UserInDB = Depends(get_current_user)):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     
@@ -102,18 +105,20 @@ async def get_current_active_user(current_user: UserInDB = Depends(get_current_u
 
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = authenticate_user(db, form_data.username, form_data.password)
+    user = authenticate_user(db=db, username=form_data.username, password=form_data.password)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username or password", headers={"WWW-Authenticate": "Bearer"})
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Incorrect username or password: {user}", headers={"WWW-Authenticate": "Bearer"})
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
 
     return {"access_token": access_token, "token_type": "bearer"}
 
+
 @app.get("/users/me", response_model=User)
-async def read_users_me(current_user: User = Depends(get_current_active_user)) -> User:
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
     return current_user
 
+
 @app.get("/users/me/items")
-async def read_own_items(current_user: User = Depends(get_current_active_user)) -> list[dict[str, Any]]:
+async def read_own_items(current_user: User = Depends(get_current_active_user)):
     return [{"item_id": 1, "owner": current_user}]
